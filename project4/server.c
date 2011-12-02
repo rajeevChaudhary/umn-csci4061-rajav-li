1d66745b4cc70fd5979e39cccb21e7de34a629dc
jrajav <jrajav@gmail.com>
2011-12-02 17:55:38 +0000

    Halfway implementing pseudocode

--- project4/server.c
+++ project4/server.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "util.h"
@@ -24,24 +25,297 @@ enum mode {
 };
 
 
+
+
+
+
+
+
+// MurmurHash 2 algorithm created by Austin Appleby. Code is in the public domain.
+// (Current version is MurmurHash 3; This version is simpler.)
+uint32_t MurmurHash2 ( const void * key, int len, uint32_t seed )
+{
+  // 'm' and 'r' are mixing constants generated offline.
+  // They're not really 'magic', they just happen to work well.
+
+  const uint32_t m = 0x5bd1e995;
+  const int r = 24;
+
+  // Initialize the hash to a 'random' value
+
+  uint32_t h = seed ^ len;
+
+  // Mix 4 bytes at a time into the hash
+
+  const unsigned char * data = (const unsigned char *)key;
+
+  while(len >= 4)
+  {
+    uint32_t k = *(uint32_t*)data;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+
+    h *= m;
+    h ^= k;
+
+    data += 4;
+    len -= 4;
+  }
+
+  // Handle the last few bytes of the input array
+
+  switch(len)
+  {
+  case 3: h ^= data[2] << 16;
+  case 2: h ^= data[1] << 8;
+  case 1: h ^= data[0];
+      h *= m;
+  };
+
+  // Do a few final mixes of the hash to ensure the last few
+  // bytes are well-incorporated.
+
+  h ^= h >> 13;
+  h *= m;
+  h ^= h >> 15;
+
+  return h;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 struct request {
     int fd;
 	char filename[FILENAME_SIZE];
+	intmax_t filesize;
 
 	struct request* next;
+    struct request* prev;
 };
 
 struct cache_entry {
     char filename[FILENAME_SIZE];
-    char* data;
+    char* filedata;
+    intmax_t filesize;
 
     struct cache_entry* next;
+    struct cache_entry* prev;
+
+    struct cache_entry* next_collision;
+    struct cache_entry* prev_collision;
 };
 
 struct cached_request {
-	struct request req;
-	char* data;
+	struct request* req;
+	char* filedata;
 };
+
+struct request *queue_sentinel;
+unsigned queue_size;
+
+struct request *prefetch_sentinel;
+unsigned prefetch_size;
+
+struct cache_entry *cache_sentinel;
+struct cache_entry *hash_table; // Static array of size max_cache_size
+unsigned cache_size;
+unsigned max_cache_size; // Set by user, will not exceed MAX_CACHE_SIZE
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LIST-ADD, LIST-REMOVE, and LIST-SHIFT defined on the queue
+struct request* q_add(int fd, const char* const filename, intmax_t filesize) {
+
+	struct request* r = malloc(sizeof(struct request));
+
+	r->fd = fd;
+	strncpy(r->filename, filename, FILENAME_SIZE);
+	r->filesize = filesize;
+
+	queue_sentinel->prev->next = r;
+	r->prev = queue_sentinel->prev;
+	queue_sentinel->prev = r;
+	r->next = queue_sentinel;
+
+	++queue_size;
+
+	return r;
+}
+
+void q_remove(struct request* r) {
+
+	if (r != queue_sentinel) {
+		r->prev->next = r->next;
+		r->next->prev = r->prev;
+
+		--queue_size;
+	}
+
+}
+
+struct request* q_shift() {
+
+	struct request* head = queue_sentinel->next;
+
+	if (head != queue_sentinel) {
+		q_remove(head);
+		return head;
+	}
+	else
+		return NULL;
+}
+
+
+
+
+// LIST-ADD, LIST-REMOVE, and LIST-SHIFT defined on the prefetch queue
+struct request* p_add(struct request* r) {
+
+	prefetch_sentinel->prev->next = r;
+	r->prev = prefetch_sentinel->prev;
+	prefetch_sentinel->prev = r;
+	r->next = prefetch_sentinel;
+
+	++prefetch_size;
+
+	return r;
+}
+
+void p_remove(struct request* r) {
+
+	if (r != prefetch_sentinel) {
+		r->prev->next = r->next;
+		r->next->prev = r->prev;
+
+		--prefetch_size;
+	}
+
+}
+
+struct request* p_shift() {
+
+	struct request* head = prefetch_sentinel->next;
+
+	if (head != prefetch_sentinel) {
+		q_remove(head);
+		return head;
+	}
+	else
+		return NULL;
+}
+
+
+
+
+
+
+//HASHED-LIST functions for the cache
+void hash_add(struct cache_entry* e) {
+	e->next_collision = NULL;
+	e->prev_collision = NULL;
+
+	unsigned hash = MurmurHash2(e->filename, 1024, 436) % 100;
+
+	if (hash_table[hash] == NULL) {
+		hash_table[hash] = e;
+	} else {
+		struct cache_entry* current = hash_table[hash];
+		while (current->next_collision != NULL)
+			current = current->next_collision;
+		current->next_collision = e;
+		e->prev_collision = current;
+	}
+}
+
+struct cache_entry* c_add(const char* const filename, const char* const filedata, intmax_t filesize) {
+
+	struct cache_entry* e = malloc(sizeof(struct cache_entry));
+
+	strncpy(e->filename, filename, 1024);
+	e->filedata = filedata;
+	e->filesize = filesize;
+
+	cache_sentinel->prev->next = e;
+	e->prev = cache_sentinel->prev;
+	cache_sentinel->prev = e;
+	e->next = cache_sentinel;
+
+	++cache_size;
+
+	hash_add(e);
+
+	return e;
+}
+
+void hash_remove(struct cache_entry* e) {
+
+	if (e->prev_collision == NULL) {
+		unsigned hash = MurmurHash2(e->filename, 1024, 436) % 100;
+
+		if (e->next_collision == NULL) // This hash is now empty
+			hash_table[hash] = NULL;
+		else
+			hash_table[hash] = e->next_collision;
+	} else {
+		e->prev_collision->next_collision = e->next_collision;
+		if (e->next_collision != NULL)
+			e->next_collision->prev_collision = e->prev_collision;
+	}
+
+}
+
+void c_remove(struct cache_entry* e) {
+
+	if (e != cache_sentinel) {
+		e->prev->next = e->next;
+		e->next->prev = e->prev;
+
+		--cache_size;
+	}
+
+	hash_remove(e);
+
+}
+
+struct cache_entry* c_shift() {
+
+	struct cache_entry* head = cache_sentinel->next;
+
+	if (head != cache_sentinel) {
+		list_remove(head);
+		return head;
+	}
+	else
+		return NULL;
+}
+
+
+
+
+
+
+
 
 
 
@@ -52,7 +326,6 @@ struct cached_request {
 struct cache_entry* cache_head = NULL;
 struct cache_entry* cache_tail = NULL;
 unsigned current_cache_size = 0;
-int max_cache_size; // Set by user, will not exceed MAX_CACHE_SIZE
 
 pthread_mutex_t cache_mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -333,6 +606,20 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: web_server_http <port> <path> <num_dispatch> <num_workers> <num_prefetch> <qlen> <mode> <cache_entries>\n");
         exit(1);
     }
+
+
+
+
+    queue_sentinel = malloc( sizeof(struct request) );
+    queue_sentinel->next = &queue_sentinel;
+    queue_sentinel->prev = &queue_sentinel;
+
+    prefetch_sentinel = malloc( sizeof(struct request) );
+    prefetch_sentinel->next = &prefetch_sentinel;
+    prefetch_sentinel->prev = &prefetch_sentinel;
+
+
+
 
     // argv[1] := <port>
     // Port number on which to accept incoming connections
